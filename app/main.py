71ad3c18855824ceffca30c006734dccbcd4bee7
Farzam <farzamgt@gmail.com>
2025-06-18 12:28:43 +0300

    Fixed by review

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-from typing import Callable, Dict, Tuple, Any
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache_storage: Dict[Tuple, Any] = {}
+    cache_storage = {}
 
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cache_storage:
             print("Getting from cache")
             return cache_storage[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_storage[key] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_storage[key] = result
+        return result
 
     return wrapper
